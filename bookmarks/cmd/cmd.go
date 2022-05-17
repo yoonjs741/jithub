bb4ec614324f5f9e48aeef57afd56dbec95d27b3
JiSoo Yun <jsyun@monitorapp.com>
2022-05-17 09:37:34 +0900

    add boomarks/cmd

---
+++ bookmarks/cmd/cmd.go
@@ -0,0 +1,27 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func CMDPsWithPipes() {
+	binName := os.Getenv("BIN_NAME")
+	_ = binName
+
+	grep := exec.Command("grep", binName+"3")
+	ps := exec.Command("ps", "-ef")
+
+	pipe, _ := ps.StdoutPipe()
+	defer pipe.Close()
+
+	grep.Stdin = pipe
+
+	ps.Start()
+
+	res, _ := grep.Output()
+
+	fmt.Println(res)
+	fmt.Println(string(res))
+}
